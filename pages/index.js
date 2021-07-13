65cbedfeafed5af3cc782db5bda6fa09defa4e0d
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-07-13 16:21:36 -0300

    CHECKPOINT TRAINING

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import MainGrid from "../src/components/MainGrid";
+import Box from '../src/components/Box';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: "profileArea" }}>
+        <Box>
+          <img src="https://github.com/LFelpsDev.png"/>
+        </Box>
+      </div>
+
+      <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+        <Box>
+          Bem Vindo
+        </Box>
+      </div>
+
+      <div className="profileRelationsArea" style={{ gridArea: "profileRelationsArea" }}>
+        <Box>Pessoas da Comunidade</Box>
+        <Box>Comunidade</Box>
+      </div>
+    </MainGrid>
+  );
 }
---
+++ src/components/Box/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+  background: #fff;
+  border-radius: 8px;
+`;
+
+
+export default Box;
